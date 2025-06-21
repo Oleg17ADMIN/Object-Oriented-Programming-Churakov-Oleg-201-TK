ba9851e51fbd71f4a02c051196114a8ad8df57d3
Oleg17ADMIN <159014034+Oleg17ADMIN@users.noreply.github.com>
2025-06-21 23:24:28 +0300

    Add files via upload

---
+++ LabChurakov4/LabChurakov4/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace LabChurakov4
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label labelN;
+        private System.Windows.Forms.NumericUpDown nudN;
+        private System.Windows.Forms.Button btnLoad1D;
+        private System.Windows.Forms.DataGridView dgv1D;
+        private System.Windows.Forms.Button btnTask1;
+        private System.Windows.Forms.TextBox txtOutput1;
+
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label labelM;
+        private System.Windows.Forms.NumericUpDown nudM;
+        private System.Windows.Forms.Label labelN2;
+        private System.Windows.Forms.NumericUpDown nudN2;
+        private System.Windows.Forms.Button btnLoad2D;
+        private System.Windows.Forms.DataGridView dgv2D;
+        private System.Windows.Forms.Button btnTask2;
+        private System.Windows.Forms.TextBox txtOutput2;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.labelN = new System.Windows.Forms.Label();
+            this.nudN = new System.Windows.Forms.NumericUpDown();
+            this.btnLoad1D = new System.Windows.Forms.Button();
+            this.dgv1D = new System.Windows.Forms.DataGridView();
+            this.btnTask1 = new System.Windows.Forms.Button();
+            this.txtOutput1 = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.labelM = new System.Windows.Forms.Label();
+            this.nudM = new System.Windows.Forms.NumericUpDown();
+            this.labelN2 = new System.Windows.Forms.Label();
+            this.nudN2 = new System.Windows.Forms.NumericUpDown();
+            this.btnLoad2D = new System.Windows.Forms.Button();
+            this.dgv2D = new System.Windows.Forms.DataGridView();
+            this.btnTask2 = new System.Windows.Forms.Button();
+            this.txtOutput2 = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudN)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1D)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudM)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudN2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv2D)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(760, 540);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.labelN);
+            this.tabPage1.Controls.Add(this.nudN);
+            this.tabPage1.Controls.Add(this.btnLoad1D);
+            this.tabPage1.Controls.Add(this.dgv1D);
+            this.tabPage1.Controls.Add(this.btnTask1);
+            this.tabPage1.Controls.Add(this.txtOutput1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(752, 514);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Одновимірний масив";
+            // 
+            // labelN
+            // 
+            this.labelN.Location = new System.Drawing.Point(10, 15);
+            this.labelN.Name = "labelN";
+            this.labelN.Size = new System.Drawing.Size(50, 20);
+            this.labelN.TabIndex = 0;
+            this.labelN.Text = "n =";
+            // 
+            // nudN
+            // 
+            this.nudN.Location = new System.Drawing.Point(60, 12);
+            this.nudN.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudN.Name = "nudN";
+            this.nudN.Size = new System.Drawing.Size(120, 20);
+            this.nudN.TabIndex = 1;
+            this.nudN.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // btnLoad1D
+            // 
+            this.btnLoad1D.Location = new System.Drawing.Point(211, 9);
+            this.btnLoad1D.Name = "btnLoad1D";
+            this.btnLoad1D.Size = new System.Drawing.Size(100, 25);
+            this.btnLoad1D.TabIndex = 2;
+            this.btnLoad1D.Text = "Завантажити";
+            this.btnLoad1D.Click += new System.EventHandler(this.btnLoad1D_Click);
+            // 
+            // dgv1D
+            // 
+            this.dgv1D.Location = new System.Drawing.Point(10, 50);
+            this.dgv1D.Name = "dgv1D";
+            this.dgv1D.Size = new System.Drawing.Size(730, 200);
+            this.dgv1D.TabIndex = 3;
+            // 
+            // btnTask1
+            // 
+            this.btnTask1.Location = new System.Drawing.Point(10, 260);
+            this.btnTask1.Name = "btnTask1";
+            this.btnTask1.Size = new System.Drawing.Size(150, 30);
+            this.btnTask1.TabIndex = 4;
+            this.btnTask1.Text = "Обчислити Task 1";
+            this.btnTask1.Click += new System.EventHandler(this.btnTask1_Click);
+            // 
+            // txtOutput1
+            // 
+            this.txtOutput1.Location = new System.Drawing.Point(10, 300);
+            this.txtOutput1.Multiline = true;
+            this.txtOutput1.Name = "txtOutput1";
+            this.txtOutput1.ReadOnly = true;
+            this.txtOutput1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput1.Size = new System.Drawing.Size(730, 200);
+            this.txtOutput1.TabIndex = 5;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.labelM);
+            this.tabPage2.Controls.Add(this.nudM);
+            this.tabPage2.Controls.Add(this.labelN2);
+            this.tabPage2.Controls.Add(this.nudN2);
+            this.tabPage2.Controls.Add(this.btnLoad2D);
+            this.tabPage2.Controls.Add(this.dgv2D);
+            this.tabPage2.Controls.Add(this.btnTask2);
+            this.tabPage2.Controls.Add(this.txtOutput2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Size = new System.Drawing.Size(752, 514);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Двовимірний масив";
+            // 
+            // labelM
+            // 
+            this.labelM.Location = new System.Drawing.Point(10, 15);
+            this.labelM.Name = "labelM";
+            this.labelM.Size = new System.Drawing.Size(60, 20);
+            this.labelM.TabIndex = 0;
+            this.labelM.Text = "m =";
+            // 
+            // nudM
+            // 
+            this.nudM.Location = new System.Drawing.Point(70, 12);
+            this.nudM.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.nudM.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudM.Name = "nudM";
+            this.nudM.Size = new System.Drawing.Size(120, 20);
+            this.nudM.TabIndex = 1;
+            this.nudM.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // labelN2
+            // 
+            this.labelN2.Location = new System.Drawing.Point(160, 15);
+            this.labelN2.Name = "labelN2";
+            this.labelN2.Size = new System.Drawing.Size(60, 20);
+            this.labelN2.TabIndex = 2;
+            this.labelN2.Text = "n =";
+            // 
+            // nudN2
+            // 
+            this.nudN2.Location = new System.Drawing.Point(220, 12);
+            this.nudN2.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.nudN2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudN2.Name = "nudN2";
+            this.nudN2.Size = new System.Drawing.Size(120, 20);
+            this.nudN2.TabIndex = 3;
+            this.nudN2.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // btnLoad2D
+            // 
+            this.btnLoad2D.Location = new System.Drawing.Point(384, 9);
+            this.btnLoad2D.Name = "btnLoad2D";
+            this.btnLoad2D.Size = new System.Drawing.Size(100, 25);
+            this.btnLoad2D.TabIndex = 4;
+            this.btnLoad2D.Text = "Завантажити";
+            this.btnLoad2D.Click += new System.EventHandler(this.btnLoad2D_Click);
+            // 
+            // dgv2D
+            // 
+            this.dgv2D.Location = new System.Drawing.Point(10, 50);
+            this.dgv2D.Name = "dgv2D";
+            this.dgv2D.Size = new System.Drawing.Size(730, 200);
+            this.dgv2D.TabIndex = 5;
+            // 
+            // btnTask2
+            // 
+            this.btnTask2.Location = new System.Drawing.Point(10, 260);
+            this.btnTask2.Name = "btnTask2";
+            this.btnTask2.Size = new System.Drawing.Size(150, 30);
+            this.btnTask2.TabIndex = 6;
+            this.btnTask2.Text = "Обчислити Task 2";
+            this.btnTask2.Click += new System.EventHandler(this.btnTask2_Click);
+            // 
+            // txtOutput2
+            // 
+            this.txtOutput2.Location = new System.Drawing.Point(10, 300);
+            this.txtOutput2.Multiline = true;
+            this.txtOutput2.Name = "txtOutput2";
+            this.txtOutput2.ReadOnly = true;
+            this.txtOutput2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput2.Size = new System.Drawing.Size(730, 200);
+            this.txtOutput2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Лабораторна: Масиви";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudN)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1D)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudM)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudN2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv2D)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+    }
+
+}
+
---
+++ LabChurakov4/LabChurakov4/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabChurakov4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Завантажити 1D-масив (Task1)
+        private void btnLoad1D_Click(object sender, EventArgs e)
+        {
+            int n = (int)nudN.Value;
+            dgv1D.Columns.Clear();
+            dgv1D.Rows.Clear();
+            dgv1D.ColumnCount = 1;
+            dgv1D.Columns[0].Name = "Value";
+            dgv1D.RowCount = n;
+        }
+
+        // Обчислює Task1
+        private void btnTask1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int n = (int)nudN.Value;
+                double[] arr = new double[n];
+                for (int i = 0; i < n; i++)
+                    arr[i] = Convert.ToDouble(dgv1D[0, i].Value ?? 0);
+
+                // а) кількість від’ємних
+                int negCount = arr.Count(x => x < 0);
+
+                // б) знайти індекс мінімального за модулем
+                int minIdx = arr
+                    .Select((v, idx) => new { Abs = Math.Abs(v), idx })
+                    .OrderBy(x => x.Abs)
+                    .First().idx;
+
+                // сума модулів після мінімального за модулем
+                double sumAfter = arr
+                    .Skip(minIdx + 1)
+                    .Sum(x => Math.Abs(x));
+
+                // замінити від’ємні на їх квадрати і відсортувати
+                double[] transformed = arr
+                    .Select(x => x < 0 ? x * x : x)
+                    .OrderBy(x => x)
+                    .ToArray();
+
+                // Вивід результатів
+                var sb = new StringBuilder();
+                sb.AppendLine($"1а) Кількість від’ємних елементів: {negCount}");
+                sb.AppendLine($"1б) Мінімальний за модулем індекс: {minIdx}");
+                sb.AppendLine($"    Сума модулів після нього: {sumAfter:F3}");
+                sb.AppendLine("Преобразований та відсортований масив:");
+                sb.AppendLine(string.Join("  ", transformed.Select(x => x.ToString("F3"))));
+
+                txtOutput1.Text = sb.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Помилка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Завантажити 2D-масив (Task2)
+        private void btnLoad2D_Click(object sender, EventArgs e)
+        {
+            int m = (int)nudM.Value;
+            int n = (int)nudN2.Value;
+            dgv2D.Columns.Clear();
+            dgv2D.Rows.Clear();
+            dgv2D.ColumnCount = n;
+            for (int j = 0; j < n; j++)
+                dgv2D.Columns[j].Name = $"C{j}";
+            dgv2D.RowCount = m;
+        }
+
+        // Обчислює Task2
+        private void btnTask2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int m = (int)nudM.Value;
+                int n = (int)nudN2.Value;
+                double[,] mat = new double[m, n];
+                for (int i = 0; i < m; i++)
+                    for (int j = 0; j < n; j++)
+                        mat[i, j] = Convert.ToDouble(dgv2D[j, i].Value ?? 0);
+
+                var sb = new StringBuilder();
+                sb.AppendLine("Початковий масив:");
+                sb.Append(PrintMatrix(mat));
+
+                // a) swap (0, n-1) ↔ (m-1, 0)
+                Swap(mat, 0, n - 1, m - 1, 0);
+                // b) swap (m-1, n-1) ↔ (0, 0)
+                Swap(mat, m - 1, n - 1, 0, 0);
+
+                sb.AppendLine("\nПісля перестановки кутів:");
+                sb.Append(PrintMatrix(mat));
+
+                txtOutput2.Text = sb.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Помилка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Допоміжні методи
+        private void Swap(double[,] a, int i1, int j1, int i2, int j2)
+        {
+            var tmp = a[i1, j1];
+            a[i1, j1] = a[i2, j2];
+            a[i2, j2] = tmp;
+        }
+
+        private string PrintMatrix(double[,] a)
+        {
+            var sb = new StringBuilder();
+            int m = a.GetLength(0), n = a.GetLength(1);
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    sb.Append($"{a[i, j],8:F2}");
+                sb.AppendLine();
+            }
+            return sb.ToString();
+        }
+    }
+
+}
