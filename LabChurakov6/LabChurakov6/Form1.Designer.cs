1a4c2be6f5cb01a6bef23d2e65e5e3c66f0fa061
Oleg17ADMIN <159014034+Oleg17ADMIN@users.noreply.github.com>
2025-06-21 23:31:53 +0300

    Add files via upload

---
+++ LabChurakov6/LabChurakov6/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace LabChurakov6
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label lblReA;
+        private System.Windows.Forms.Label lblImA;
+        private System.Windows.Forms.TextBox txtReA;
+        private System.Windows.Forms.TextBox txtImA;
+
+        private System.Windows.Forms.Label lblReB;
+        private System.Windows.Forms.Label lblImB;
+        private System.Windows.Forms.TextBox txtReB;
+        private System.Windows.Forms.TextBox txtImB;
+
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.TextBox txtResult;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblReA = new System.Windows.Forms.Label();
+            this.lblImA = new System.Windows.Forms.Label();
+            this.txtReA = new System.Windows.Forms.TextBox();
+            this.txtImA = new System.Windows.Forms.TextBox();
+            this.lblReB = new System.Windows.Forms.Label();
+            this.lblImB = new System.Windows.Forms.Label();
+            this.txtReB = new System.Windows.Forms.TextBox();
+            this.txtImB = new System.Windows.Forms.TextBox();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+
+            // 
+            // lblReA
+            // 
+            this.lblReA.Location = new System.Drawing.Point(12, 15);
+            this.lblReA.Size = new System.Drawing.Size(80, 20);
+            this.lblReA.Text = "A.Real =";
+            // 
+            // txtReA
+            // 
+            this.txtReA.Location = new System.Drawing.Point(100, 12);
+            this.txtReA.Size = new System.Drawing.Size(100, 22);
+            // 
+            // lblImA
+            // 
+            this.lblImA.Location = new System.Drawing.Point(12, 45);
+            this.lblImA.Size = new System.Drawing.Size(80, 20);
+            this.lblImA.Text = "A.Imag =";
+            // 
+            // txtImA
+            // 
+            this.txtImA.Location = new System.Drawing.Point(100, 42);
+            this.txtImA.Size = new System.Drawing.Size(100, 22);
+            // 
+            // lblReB
+            // 
+            this.lblReB.Location = new System.Drawing.Point(12, 85);
+            this.lblReB.Size = new System.Drawing.Size(80, 20);
+            this.lblReB.Text = "B.Real =";
+            // 
+            // txtReB
+            // 
+            this.txtReB.Location = new System.Drawing.Point(100, 82);
+            this.txtReB.Size = new System.Drawing.Size(100, 22);
+            // 
+            // lblImB
+            // 
+            this.lblImB.Location = new System.Drawing.Point(12, 115);
+            this.lblImB.Size = new System.Drawing.Size(80, 20);
+            this.lblImB.Text = "B.Imag =";
+            // 
+            // txtImB
+            // 
+            this.txtImB.Location = new System.Drawing.Point(100, 112);
+            this.txtImB.Size = new System.Drawing.Size(100, 22);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Location = new System.Drawing.Point(12, 150);
+            this.btnDivide.Size = new System.Drawing.Size(188, 30);
+            this.btnDivide.Text = "Ділити A ÷ B";
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(12, 195);
+            this.txtResult.Size = new System.Drawing.Size(380, 50);
+            this.txtResult.Multiline = true;
+            this.txtResult.ReadOnly = true;
+            this.txtResult.BackColor = System.Drawing.SystemColors.Window;
+
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(404, 260);
+            this.Controls.Add(this.lblReA);
+            this.Controls.Add(this.txtReA);
+            this.Controls.Add(this.lblImA);
+            this.Controls.Add(this.txtImA);
+            this.Controls.Add(this.lblReB);
+            this.Controls.Add(this.txtReB);
+            this.Controls.Add(this.lblImB);
+            this.Controls.Add(this.txtImB);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.txtResult);
+            this.Text = "Ділення комплексів";
+        }
+    }
+
+}
+
---
+++ LabChurakov6/LabChurakov6/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabChurakov6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Структура для комплексного числа
+        private struct ComplexPair
+        {
+            public double Real;
+            public double Imag;
+            public ComplexPair(double r, double i)
+            {
+                Real = r; Imag = i;
+            }
+            public override string ToString()
+            {
+                string sign = Imag >= 0 ? "+" : "-";
+                return $"{Real} {sign} {Math.Abs(Imag)}i";
+            }
+        }
+
+        // Метод ділення з обробкою виключень
+        private ComplexPair DivideComplex(ComplexPair a, ComplexPair b)
+        {
+            ComplexPair result = new ComplexPair(0, 0);
+            try
+            {
+                double denom = b.Real * b.Real + b.Imag * b.Imag;
+                if (denom == 0)
+                    throw new DivideByZeroException("Ділення на нульовий комплекс!");
+
+                double realPart = (a.Real * b.Real + a.Imag * b.Imag) / denom;
+                double imagPart = (a.Imag * b.Real - a.Real * b.Imag) / denom;
+                result = new ComplexPair(realPart, imagPart);
+            }
+            catch (DivideByZeroException dz)
+            {
+                MessageBox.Show(dz.Message, "Помилка ділення",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Невідома помилка: " + ex.Message,
+                    "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                // Тут можна додати логування або інші дії,
+                // які мають виконатися в будь-якому разі.
+                Console.WriteLine("DivideComplex викликаний.");
+            }
+            return result;
+        }
+
+        // Обробник кнопки
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            txtResult.Clear();
+            try
+            {
+                var a = new ComplexPair(
+                    Convert.ToDouble(txtReA.Text),
+                    Convert.ToDouble(txtImA.Text)
+                );
+                var b = new ComplexPair(
+                    Convert.ToDouble(txtReB.Text),
+                    Convert.ToDouble(txtImB.Text)
+                );
+
+                ComplexPair res = DivideComplex(a, b);
+                txtResult.Text = $"A / B = {res}";
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Неправильний формат числа!",
+                    "Помилка вводу", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка: " + ex.Message,
+                    "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                // Повернемо фокус на перше поле вводу
+                txtReA.Focus();
+            }
+        }
+    }
+
+}
