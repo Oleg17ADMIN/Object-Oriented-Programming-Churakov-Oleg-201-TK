f63a10d1df254814182a66529d087969840dbf4d
Oleg17ADMIN <159014034+Oleg17ADMIN@users.noreply.github.com>
2025-06-24 22:04:59 +0300

    Add files via upload

---
+++ Lab9/Lab9/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Lab9
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.PictureBox picBoxOutput;
+        private System.Windows.Forms.TextBox txtInputP;
+        private System.Windows.Forms.TextBox txtInputTmin;
+        private System.Windows.Forms.TextBox txtInputTmax;
+        private System.Windows.Forms.Button BtnOK;
+        private System.Windows.Forms.Label lblP;
+        private System.Windows.Forms.Label lblTmin;
+        private System.Windows.Forms.Label lblTmax;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.picBoxOutput = new System.Windows.Forms.PictureBox();
+            this.txtInputP = new System.Windows.Forms.TextBox();
+            this.txtInputTmin = new System.Windows.Forms.TextBox();
+            this.txtInputTmax = new System.Windows.Forms.TextBox();
+            this.BtnOK = new System.Windows.Forms.Button();
+            this.lblP = new System.Windows.Forms.Label();
+            this.lblTmin = new System.Windows.Forms.Label();
+            this.lblTmax = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxOutput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBoxOutput
+            // 
+            this.picBoxOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picBoxOutput.Location = new System.Drawing.Point(12, 12);
+            this.picBoxOutput.Name = "picBoxOutput";
+            this.picBoxOutput.Size = new System.Drawing.Size(500, 400);
+            this.picBoxOutput.TabIndex = 0;
+            this.picBoxOutput.TabStop = false;
+            // 
+            // txtInputP
+            // 
+            this.txtInputP.Location = new System.Drawing.Point(550, 30);
+            this.txtInputP.Name = "txtInputP";
+            this.txtInputP.Size = new System.Drawing.Size(100, 20);
+            // 
+            // txtInputTmin
+            // 
+            this.txtInputTmin.Location = new System.Drawing.Point(550, 70);
+            this.txtInputTmin.Name = "txtInputTmin";
+            this.txtInputTmin.Size = new System.Drawing.Size(100, 20);
+            // 
+            // txtInputTmax
+            // 
+            this.txtInputTmax.Location = new System.Drawing.Point(550, 110);
+            this.txtInputTmax.Name = "txtInputTmax";
+            this.txtInputTmax.Size = new System.Drawing.Size(100, 20);
+            // 
+            // BtnOK
+            // 
+            this.BtnOK.Location = new System.Drawing.Point(550, 150);
+            this.BtnOK.Name = "BtnOK";
+            this.BtnOK.Size = new System.Drawing.Size(100, 30);
+            this.BtnOK.Text = "Побудувати";
+            this.BtnOK.Click += new System.EventHandler(this.BtnOK_Click);
+            // 
+            // Labels
+            // 
+            this.lblP.Text = "p:";
+            this.lblP.Location = new System.Drawing.Point(520, 30);
+
+            this.lblTmin.Text = "t min:";
+            this.lblTmin.Location = new System.Drawing.Point(500, 70);
+
+            this.lblTmax.Text = "t max:";
+            this.lblTmax.Location = new System.Drawing.Point(500, 110);
+
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(680, 430);
+            this.Controls.Add(this.picBoxOutput);
+            this.Controls.Add(this.txtInputP);
+            this.Controls.Add(this.txtInputTmin);
+            this.Controls.Add(this.txtInputTmax);
+            this.Controls.Add(this.BtnOK);
+            this.Controls.Add(this.lblP);
+            this.Controls.Add(this.lblTmin);
+            this.Controls.Add(this.lblTmax);
+            this.Name = "Form1";
+            this.Text = "Параметричний графік";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxOutput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ Lab9/Lab9/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lab9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            picBoxOutput.Image = new Bitmap(picBoxOutput.Width, picBoxOutput.Height);
+            DrawAxes();
+        }
+
+        private void DrawAxes()
+        {
+            Graphics g = Graphics.FromImage(picBoxOutput.Image);
+            g.Clear(Color.White);
+
+            Pen axisPen = new Pen(Color.Black, 2);
+            Pen gridPen = new Pen(Color.LightGray, 1);
+            Font font = new Font("Arial", 8);
+            Font axisFont = new Font("Arial", 10, FontStyle.Bold);
+            Brush brush = Brushes.Black;
+
+            int w = picBoxOutput.Width;
+            int h = picBoxOutput.Height;
+            int cx = w / 2;
+            int cy = h / 2;
+
+            // Сітка
+            for (int i = 0; i < w; i += 20)
+                g.DrawLine(gridPen, i, 0, i, h);
+            for (int i = 0; i < h; i += 20)
+                g.DrawLine(gridPen, 0, i, w, i);
+
+            // Осі
+            g.DrawLine(axisPen, 0, cy, w, cy); // вісь X
+            g.DrawLine(axisPen, cx, 0, cx, h); // вісь Y
+
+            // Написи для осей
+            g.DrawString("x", axisFont, brush, w - 15, cy + 5); // підпис X
+            g.DrawString("y", axisFont, brush, cx + 5, 5);       // підпис Y
+
+            // Цифри
+            for (int i = -10; i <= 10; i++)
+            {
+                if (i == 0) continue;
+                g.DrawString(i.ToString(), font, brush, cx + i * 20 - 5, cy + 2); // x-мітки
+                g.DrawString((-i).ToString(), font, brush, cx + 2, cy + i * 20 - 5); // y-мітки
+            }
+
+            picBoxOutput.Invalidate();
+        }
+
+
+        private void BtnOK_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DrawAxes();
+                Graphics g = Graphics.FromImage(picBoxOutput.Image);
+                Pen curvePen = new Pen(Color.Red, 2);
+
+                int w = picBoxOutput.Width;
+                int h = picBoxOutput.Height;
+                int cx = w / 2;
+                int cy = h / 2;
+
+                double p = Convert.ToDouble(txtInputP.Text);
+                double tMin = Convert.ToDouble(txtInputTmin.Text);
+                double tMax = Convert.ToDouble(txtInputTmax.Text);
+
+                if (tMax <= tMin)
+                {
+                    MessageBox.Show("t_max має бути більшим за t_min.");
+                    return;
+                }
+
+                int steps = 500;
+                PointF[] points = new PointF[steps];
+                for (int i = 0; i < steps; i++)
+                {
+                    double t = tMin + i * (tMax - tMin) / (steps - 1);
+                    double x = t;
+                    double y = Math.Sqrt(2 * p * t);
+
+                    float sx = (float)(cx + x * 20);
+                    float sy = (float)(cy - y * 20);
+                    points[i] = new PointF(sx, sy);
+                }
+
+                g.DrawLines(curvePen, points);
+                picBoxOutput.Invalidate();
+            }
+            catch
+            {
+                MessageBox.Show("Перевірте правильність введення чисел.");
+            }
+        }
+    }
+}
